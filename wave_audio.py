78af0da3979e07902748bab354de67e76a50e1c3
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-08-06 21:06:36 +0530

    Create wave_audio.py

---
+++ wave_audio.py
@@ -0,0 +1,30 @@
+import wave
+obj = wave.open("testaudio.wav","rb")
+#Getter functions
+print("Number of channels: ",obj.getnchannels())
+print("Sample width: ",obj.getsampwidth())
+print("Framerate: ",obj.getframerate())
+print("Number of frames: ",obj.getnframes)
+print("Parameters: ",obj.getparams())
+
+
+t_audio = obj.getnframes()/obj.getframerate()#this is used to get the duration of the audio
+print("Duration of the audio: ",t_audio)
+
+
+
+frames = obj.readframes(-1)
+print(type(frames),type(frames[0]))#This will return 'byte', since byte is made up of integers, frames[n] will return int
+print(len(frames))
+obj.close()
+
+
+#To save the data
+obj_new = wave.open("newtestaudio.wav","wb")
+#Setter functions
+obj_new.setnchannels(3)
+obj_new.setsampwidth(4)
+obj_new.setframerate(30000.0)
+
+obj_new.writeframes(frames)
+obj_new.close()
